e5a4872f0a83bc006b20e122f10d245c8881a49e
sagar9836 <sagarpaal116@gmail.com>
2026-08-18 12:51:55 +0530

    adding the remote backend

--- 2_Remote_Infra/dynamodb.tf
+++ 2_Remote_Infra/dynamodb.tf
@@ -1,8 +1,6 @@
 resource "aws_dynamodb_table" "basic-dynamodb-table" {
   name           = "dynamodb-table-1"
   billing_mode   = "PAY_PER_REQUEST"
-  read_capacity  = 20
-  write_capacity = 20
   hash_key       = "LockID"
   
   attribute {
